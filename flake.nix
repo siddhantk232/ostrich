888487800a7a2ac680b913a6ea899bc11815d581
siddhantCodes <siddhantk232@gmail.com>
2023-03-07 18:05:20 +0530

    use pinned nixpkgs

--- flake.nix
+++ flake.nix
@@ -1,9 +1,6 @@
 {
   description = "ostrich: os dev for learning purpose";
 
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-
-
   outputs = { self, nixpkgs }:
   let pkgs = nixpkgs.legacyPackages.x86_64-linux;
   in
